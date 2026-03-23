2a1cf78b744b5171b4fe2ed38a45d535f46e2781
support-pra <support@pra.ac.th>
2026-03-23 10:11:13 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+const APP_CONFIG = {
+    // 🛑 นำ Web App URL ที่ได้จาก Google Apps Script มาวางที่นี่
+    API_URL: "ใส่_URL_ที่ได้จาก_Apps_Script_ตรงนี้",
+    
+    // ตั้งค่าข้อความต่างๆ บนหน้าเว็บ
+    TITLE_MAIN: "🎯 ระบบแสดงผลคะแนนสอบรายบุคคล",
+    SUB_TITLE: "✨ นักเรียนห้องเรียนปกติ และนักเรียนความสามารถพิเศษ ✨<br>ระดับชั้นมัธยมศึกษาปีที่ 1 และ 4 ปีการศึกษา 2569",
+    SCHOOL_NAME: "โรงเรียนปราจิณราษฎรอำรุง",
+    AREA_NAME: "สำนักงานเขตพื้นที่การศึกษามัธยมศึกษาปราจีนบุรี นครนายก",
+    FACEBOOK_URL: "https://www.facebook.com/prachinratsadornamroongschool?locale=th_TH",
+    FOOTER_TEXT: "© 2026 งานรับนักเรียน Create by กลุ่มงานเทคโนโลยี"
+};
